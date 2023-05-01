f7ec3af8e08b4768e0f3db5b83a48647067e899e
yamap55 <1785817+yamap55@users.noreply.github.com>
2023-05-01 09:54:33 +0900

    feat: add sample slack app

---
+++ manifest.ts
@@ -0,0 +1,17 @@
+import { Manifest } from "deno-slack-sdk/mod.ts";
+import NotifyWorkflow from "./workflows/nobiru_kun.ts";
+
+/**
+ * The app manifest contains the app's configuration. This
+ * file defines attributes like app name and description.
+ * https://api.slack.com/future/manifest
+ */
+export default Manifest({
+  name: "slack-nobiru-kun",
+  description: "A blank template for building Slack apps with Deno",
+  icon: "assets/default_new_app_icon.png",
+  functions: [],
+  workflows: [NotifyWorkflow],
+  outgoingDomains: [],
+  botScopes: ["commands", "chat:write", "chat:write.public"],
+});
