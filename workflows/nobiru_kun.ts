d87274fed0ec9e31f43e768d1a2998da1aa4066d
yamap55 <1785817+yamap55@users.noreply.github.com>
2023-05-05 16:15:29 +0900

    feat: remove channel id setting

--- workflows/nobiru_kun.ts
+++ workflows/nobiru_kun.ts
@@ -1,6 +1,5 @@
 import { DefineWorkflow, Schema } from "deno-slack-sdk/mod.ts";
 import { CreateResponseMessageFunction } from "../functions/create_response_message.ts";
-import { GetTargetChannelFunction } from "../functions/get_target_channel.ts";
 
 const NotifyWorkflow = DefineWorkflow({
   callback_id: "nobiru_kun-workflow",
@@ -8,30 +7,21 @@ const NotifyWorkflow = DefineWorkflow({
   input_parameters: {
     properties: {
       channel_id: { type: Schema.slack.types.channel_id },
-      channel_type: { type: Schema.types.string },
-      user_id: { type: Schema.slack.types.user_id },
       text: { type: Schema.types.string },
-      message_ts: { type: Schema.types.string },
-      data: { type: Schema.types.object },
     },
     required: [
       "channel_id",
-      "channel_type",
-      "user_id",
       "text",
-      "message_ts",
-      "data",
     ],
   },
 });
 
-const target_channel_id = NotifyWorkflow.addStep(GetTargetChannelFunction, {});
 const create_response_message = NotifyWorkflow.addStep(CreateResponseMessageFunction, {
-  "input_text": NotifyWorkflow.inputs.data.text,
+  "input_text": NotifyWorkflow.inputs.text,
 });
 
 NotifyWorkflow.addStep(Schema.slack.functions.SendMessage, {
-  channel_id: target_channel_id.outputs.target_channel_id,
+  channel_id: NotifyWorkflow.inputs.channel_id,
   message: create_response_message.outputs.response_message,
 });
 
